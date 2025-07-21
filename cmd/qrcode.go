5efd72894e97b94d54e7db5de4b2b4149c44a54a
@tinacious <info@tinaciousdesign.com>
2025-07-20 22:19:00 -0400

    add support for generating svg files for qrcode command

--- cmd/qrcode.go
+++ cmd/qrcode.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"bufio"
 	"fmt"
+	"image"
 	"image/png"
 	"log"
 	"os"
@@ -13,6 +14,7 @@ import (
 	"github.com/makiuchi-d/gozxing/qrcode"
 	"github.com/spf13/cobra"
 	"github.com/tinacious/random/utils"
+	"github.com/xyproto/png2svg"
 )
 
 var qrcodeCmd = &cobra.Command{
@@ -31,6 +33,14 @@ func qrCodeCmd(cmd *cobra.Command, args []string) {
 	shouldOpenFile, err := cmd.Flags().GetBool("open")
 	if err != nil {
 		log.Fatal("invalid flag: open")
+	}
+
+	fileFormat, err := cmd.Flags().GetString("format")
+	if err != nil {
+		log.Fatal("invalid flag: format")
+	}
+	if fileFormat != "png" && fileFormat != "svg" {
+		log.Fatal("invalid flag: format - must be one of: svg, png")
 	}
 
 	scn := bufio.NewScanner(os.Stdin)
@@ -55,10 +65,18 @@ func qrCodeCmd(cmd *cobra.Command, args []string) {
 		log.Fatal("no data to encode")
 	}
 
+	if fileFormat == "png" {
+		fmt.Println("✅ Got the input. Converting to PNG. Should be quick.")
+	} else {
+		fmt.Println("✅ Got the input. Converting to SVG. This might take a bit. If it's taking too long, try reducing the file size.")
+	}
+
 	enc := qrcode.NewQRCodeWriter()
 
 	now := time.Now()
-	fileName := fmt.Sprintf("qr-code_%s.png", now.Format("2006-01-02_15.04.05"))
+
+	// Filename using the file type `fileFormat` and date, e.g. qr-code_2025-07-20_21.53.51.svg
+	fileName := fmt.Sprintf("qr-code_%s.%s", now.Format("2006-01-02_15.04.05"), fileFormat)
 
 	img, err := enc.Encode(allLines, gozxing.BarcodeFormat_QR_CODE, size, size, nil)
 	if err != nil {
@@ -71,12 +89,7 @@ func qrCodeCmd(cmd *cobra.Command, args []string) {
 	}
 	defer file.Close()
 
-	// *BitMatrix implements the image.Image interface,
-	// so it is able to be passed to png.Encode directly.
-	err = png.Encode(file, img)
-	if err != nil {
-		log.Fatal("failed to create QR code file")
-	}
+	createImageFile(img, file, fileName, fileFormat)
 
 	fmt.Printf("✅ Successfully created %s\n", fileName)
 
@@ -90,9 +103,37 @@ func qrCodeCmd(cmd *cobra.Command, args []string) {
 	}
 }
 
+// createImageFile Checks the fileFormat and uses the respective library for generating an image
+// the png2svg library does not return an error, assuming it will cause a fatal exception but i haven't seen it fail 🤷🏻‍♀️
+func createImageFile(img image.Image, file *os.File, fileName string, fileFormat string) {
+	if fileFormat == "png" {
+		err := createPng(img, file)
+		if err != nil {
+			log.Fatal("failed to create QR code file as PNG")
+		}
+	} else {
+		createSvg(img, fileName)
+	}
+}
+
+func createPng(img image.Image, file *os.File) error {
+	// *BitMatrix implements the image.Image interface,
+	// so it is able to be passed to png.Encode directly.
+	return png.Encode(file, img)
+}
+
+// uses png2svg to convert the image.Image interface into SVG
+func createSvg(img image.Image, fileName string) {
+	var verbose bool = false // todo: maybe make this configurable
+	pi := png2svg.NewPixelImage(img, verbose)
+	pi.CoverAllPixels()
+	pi.WriteSVG(fileName)
+}
+
 func init() {
-	qrcodeCmd.Flags().IntP("size", "s", 1440, "How wide in pixels?")
-	qrcodeCmd.Flags().Bool("open", false, "Open the file after generating?")
+	qrcodeCmd.Flags().IntP("size", "s", 200, "How wide in pixels?")
+	qrcodeCmd.Flags().Bool("open", true, "Open the file after generating?")
+	qrcodeCmd.Flags().StringP("format", "f", "svg", "File format. Options include: svg, png")
 
 	rootCmd.AddCommand(qrcodeCmd)
 }
--- go.mod
+++ go.mod
@@ -6,8 +6,10 @@ toolchain go1.23.4
 
 require (
 	github.com/google/uuid v1.6.0
+	github.com/makiuchi-d/gozxing v0.1.1
 	github.com/spf13/cobra v1.8.1
 	github.com/stretchr/testify v1.9.0
+	github.com/xyproto/png2svg v1.6.0
 	github.com/yelinaung/go-haikunator v0.0.0-20221222235932-36bf4c441150
 	golang.org/x/text v0.16.0
 )
@@ -16,10 +18,10 @@ require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/makiuchi-d/gozxing v0.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/xyproto/tinysvg v1.1.1 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
